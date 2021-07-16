5a201f819b57fe8a501c1304458272436539ebb9
margkru <kruleva.margo@gmail.com>
2021-07-16 16:43:20 +0400

    blog headers + polls

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
-from . import views
+from polls import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('question/', views.IndexView.as_view(), name='index'),
+    path('question/<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('question/<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('question/<int:question_id>/vote/', views.vote, name='vote'),
+    path('question/create/', views.QuestionCreate.as_view(), name='create'),
+    path('question/<int:pk>/update/', views.QuestionUpdate.as_view(), name='update'),
+    path('question/<int:pk>/delete/', views.QuestionDelete.as_view(), name='delete')
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,25 +1,50 @@
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.views import generic
-from .models import Question, Choice
+
+from polls.models import Question, Choice
+
 
 class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
         q = Question.objects.order_by('-pub_date')[:5]
         return q
 
+
 class DetailView(generic.DetailView):
     template_name = 'polls/detail.html'
     model = Question
 
+
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
+
+class QuestionCreate(generic.CreateView):
+    model = Question
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse('polls:detail', kwargs={'pk': self.object.pk})
+
+
+class QuestionUpdate(generic.UpdateView):
+    model = Question
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse('polls:detail', kwargs={'pk': self.object.pk})
+
+
+class QuestionDelete(generic.DeleteView):
+    model = Question
+    success_url = reverse_lazy('polls:index')
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -32,7 +57,7 @@ def vote(request, question_id):
             'error_message': "You didn't select a choice.",
         })
     else:
-        selected_choice.vote += 1
+        selected_choice.votes += 1
         selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
